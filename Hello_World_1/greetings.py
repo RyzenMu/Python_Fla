adc7f65eb55d598d612b1542a40b28a054a46c27
RyzenMu <muruga13in2022@gmail.com>
2024-02-25 19:02:45 +0530

    --debug mode completed

--- Hello_World_1/greetings.py
+++ Hello_World_1/greetings.py
@@ -4,4 +4,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def fun():
-    return "<h1>Good Evening</h1>"
+    return '''<h1>Good Evening All</h1>
+            <p>I Am a agent</p>
+            <hr>
+            <h2> This is Heading 2 </h2>
+            <hr>
+            <h3>Super bro</h3>
+            '''
